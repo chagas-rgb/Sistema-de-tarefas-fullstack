6bce1a986e037ffcc5c08f9c458e165f4ef20dfb
Gabriel Chagas <111450073+chagas-rgb@users.noreply.github.com>
2024-11-05 23:22:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
